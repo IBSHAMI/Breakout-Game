1571c774238c9e4f20ff82d70738c1e741701667
Ibrahim <alshamiibrahim96@gmail.com>
2021-04-25 08:57:33 -0700

    Create the ball collosions with the walls

--- ball.py
+++ ball.py
@@ -11,6 +11,7 @@ X_INITIAL = 0
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
+        self.new_x, self.new_y = None, None
         self.shape("circle")
         self.color("white")
         self.penup()
@@ -19,7 +20,12 @@ class Ball(Turtle):
         self.y_move = 5
 
     def ball_moving(self):
-        new_x, new_y = self.xcor() + self.x_move, self.ycor() + self.y_move
-        self.goto(new_x, new_y)
+        self.new_x, self.new_y = self.xcor() + self.x_move, self.ycor() + self.y_move
+        self.goto(self.new_x, self.new_y)
 
+    def ball_x_wall_collosion(self):
+        self.x_move *= -1
+
+    def ball_y_wall_collosion(self):
+        self.y_move *= -1
 
--- main.py
+++ main.py
@@ -12,6 +12,15 @@ game_screen.bgcolor("black")
 game_screen.title("Arcade Game")
 game_screen.tracer(0)
 
+# X_Walls and Y_walls limits for ball collisions plus error term for better collisions
+error = 20
+X_Wall = Width/2 - error
+Y_wall = Height/2 - error
+
+
+
+
+
 paddle = Paddle()
 bricks = Bricks()
 ball = Ball()
@@ -27,6 +36,13 @@ while game_is_on:
     time.sleep(0.027)
     game_screen.update()
     ball.ball_moving()
+
+    if ball.xcor() >= X_Wall or ball.xcor() <= -X_Wall:
+        ball.ball_x_wall_collosion()
+
+    if ball.ycor() >= Y_wall:
+        ball.ball_y_wall_collosion()
+
 
 game_screen.update()
 game_screen.exitonclick()
